28d5f99619569d1f3969e066346109e59eceee12
LittlepawD <dbodnr37@gmail.com>
2020-03-16 20:30:21 +0100

    Basic structure

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import PySimpleGUI.PySimpleGUI as sg
 
+layout = None
+
+
+
+if __name__ == "__main__":
+
+    # Create window:
+    window = sg.Window("The Rebooot Program", layout, icon=None)  # TODO: add custom icon
+
+    # Mainloop:
+    while True:
+        event, values = window.read()
+        if event in (None, 'Cancel'):
+            break
+
+    # End:
+    window.close()
